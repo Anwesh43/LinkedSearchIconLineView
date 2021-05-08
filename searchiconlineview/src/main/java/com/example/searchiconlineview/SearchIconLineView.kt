e06498e4356cb1ed262573579fe2962570d74729
Anwesh43 <anweshthecool0@gmail.com>
2021-05-09 02:41:44 +0530

    adding Canvas extension methods

--- searchiconlineview/src/main/java/com/example/searchiconlineview/SearchIconLineView.kt
+++ searchiconlineview/src/main/java/com/example/searchiconlineview/SearchIconLineView.kt
@@ -7,7 +7,7 @@ import android.graphics.Color
 import android.graphics.RectF
 import android.app.Activity
 import android.content.Context
-
+import android.graphics.Canvas
 val colors : Array<Int> = arrayOf(
     "#54EE11",
     "#CC1134",
@@ -29,3 +29,33 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSearchIconLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val lineSize1 : Float = Math.min(w, h) / lineSizeFactor1
+    val lineSize2 : Float = Math.min(w, h) / lineSizeFactor2
+    val r : Float = Math.min(w, h) / rFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    save()
+    rotate(-45f * sf3)
+    drawLine(0f, 0f, 0f, -lineSize1 * sf1, paint)
+    drawArc(RectF(-r, -lineSize1 - r, r, -lineSize1 + r), 90f, 360f * sf2,false, paint)
+    restore()
+    drawLine(0f, 0f, -lineSize2 * sf4, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawSILNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawSearchIconLine(scale, w, h, paint)
+}
