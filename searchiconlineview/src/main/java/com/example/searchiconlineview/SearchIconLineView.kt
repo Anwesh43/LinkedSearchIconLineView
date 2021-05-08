a4756464ad9c5596638648cb66513502a61ad850
Anwesh43 <anweshthecool0@gmail.com>
2021-05-09 02:49:06 +0530

    addng SearchIconLineView class

--- searchiconlineview/src/main/java/com/example/searchiconlineview/SearchIconLineView.kt
+++ searchiconlineview/src/main/java/com/example/searchiconlineview/SearchIconLineView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawSILNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawSearchIconLine(scale, w, h, paint)
 }
+
+class SearchIconLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
