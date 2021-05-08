3aa79998594e878f8425bf3ea6713fbb6f6d1df6
Anwesh43 <anweshthecool0@gmail.com>
2021-05-09 02:33:20 +0530

    adding constants and libraries

--- searchiconlineview/src/main/java/com/example/searchiconlineview/SearchIconLineView.kt
+++ searchiconlineview/src/main/java/com/example/searchiconlineview/SearchIconLineView.kt
@@ -1,4 +1,26 @@
 package com.example.searchiconlineview
 
-class SearchIconLineView {
-}
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#54EE11",
+    "#CC1134",
+    "#1122CC",
+    "#DEAB12",
+    "#56ADFE"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f
+val lineSizeFactor1 : Float = 6.9f
+val rFactor : Float = 15.8f
+val lineSizeFactor2 : Float = 3.9f
+val delay : Long = 20
+val strokeFactor : Float = 90f
