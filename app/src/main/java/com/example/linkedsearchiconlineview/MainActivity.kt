afbbcc54a9789933a6eac12ac0c0d0c7521d333c
Anwesh43 <anweshthecool0@gmail.com>
2021-05-09 03:07:26 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedsearchiconlineview/MainActivity.kt
+++ app/src/main/java/com/example/linkedsearchiconlineview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedsearchiconlineview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.searchiconlineview.SearchIconLineView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SearchIconLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
