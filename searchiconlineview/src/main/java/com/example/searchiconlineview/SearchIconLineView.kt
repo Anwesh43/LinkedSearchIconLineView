112f0f9a9ba67fe0640bcec860ef5e6a759d834e
Anwesh43 <anweshthecool0@gmail.com>
2021-05-09 02:55:15 +0530

    adding SILNode class

--- searchiconlineview/src/main/java/com/example/searchiconlineview/SearchIconLineView.kt
+++ searchiconlineview/src/main/java/com/example/searchiconlineview/SearchIconLineView.kt
@@ -122,4 +122,45 @@ class SearchIconLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SILNode(var i : Int, val state : State = State()) {
+
+        private var next : SILNode? = null
+        private var prev : SILNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SILNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSILNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SILNode {
+            var curr : SILNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
